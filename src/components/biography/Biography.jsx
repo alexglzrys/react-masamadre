919a428369e018cdc585c48f033d91943ab9d442
Alejandro González Reyes <alexglzrys@gmail.com>
2023-02-27 18:40:47 -0600

    Fix componente Biography

--- src/components/biography/Biography.jsx
+++ src/components/biography/Biography.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import styles from "./Biography.module.css";
 
-function Biography({ children }) {
-  return <article className={styles.Biography}>{children}</article>;
+function Biography({ children, direction }) {
+  return (
+    <article
+      className={`${styles.Biography} ${
+        direction === "reverse" ? styles.Biography__Reverse : null
+      }`}
+    >
+      {children}
+    </article>
+  );
 }
 
 export default Biography;
