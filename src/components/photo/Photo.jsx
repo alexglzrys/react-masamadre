0ff427e0365eed91a5787fce82c3b77e4d642aea
Alejandro González Reyes <alexglzrys@gmail.com>
2023-02-27 18:40:01 -0600

    Fix componente Photo

--- src/components/photo/Photo.jsx
+++ src/components/photo/Photo.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import styles from './Photo.module.css';
 
-function Photo({ url, title }) {
+function Photo({ url, title, rotate }) {
   return (
     <img
       src={url}
       className={`${styles.Photo} wow animate__zoomIn`}
       data-wow-duration="2s"
+      style={{transform: `rotate(${rotate})`}}
       alt={title}
       title={title}
     />
